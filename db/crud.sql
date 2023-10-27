969f5ae56b9b0f22c8b1ddf96fead0090c270bcc
mattmwln <rahmatmaulanapersonal@gmail.com>
2023-10-27 17:55:29 +0700

    Tugas CRUD

---
+++ db/crud.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Okt 2023 pada 12.02
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peserta`
+--
+
+CREATE TABLE `peserta` (
+  `id_peserta` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `sekolah` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `no_hp` char(13) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `peserta`
+--
+
+INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `alamat`) VALUES
+(5, 'Rahmat Maulana', 'Ma.Sabilul Hasanah', 'IPA', '083152374889', 'Palembang'),
+(6, 'M Rohmad Nur Khoirofiq', 'Madrasah Muallimmin muallimat', 'IPS', '088286281344', 'Palembang, Sekip UJung'),
+(7, 'Feri Irawan', 'MTs.Sabilul Hasanah', '#', '08739231998', 'Palembang,Gandus');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id_peserta`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  MODIFY `id_peserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
